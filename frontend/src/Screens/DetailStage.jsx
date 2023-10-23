dbb95b11a594143a797a84a302075f6922d3cc6a
Charles Benca <zzazzbug@gmail.com>
2023-10-23 09:13:16 -0400

    commencé à faire la nouvelle page qui est dédiée à l'affichage de toutes les informations d'un stage

---
+++ frontend/src/Screens/DetailStage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+
+
+
+
+export default function DetailStage() {
+
+
+
+
+
+
+    return (
+        <div className="flex justify-center mt-8 mb-8 text-justify">
+            <h2 className="text-2xl font-bold mb-5">
+                Détail du stage
+            </h2>
+
+
+
+        </div>
+    );
+};
+
+
+
+
+
