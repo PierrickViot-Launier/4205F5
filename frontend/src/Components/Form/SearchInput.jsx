31aaa20eebce50e3e1427370489940f25fe8c107
Antonii Spasov <spasovantonii5@gmail.com>
2023-11-06 06:25:52 -0500

    Fix: Prevent user from submiting search form

--- frontend/src/Components/Form/SearchInput.jsx
+++ frontend/src/Components/Form/SearchInput.jsx
@@ -7,7 +7,7 @@ export default function SearchInput({ searchIndex, setSearchIndex }) {
     }
 
     return (
-        <form className="justify-center">
+        <form className="justify-center" onSubmit={(event) => {event.preventDefault();}}>
             <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
